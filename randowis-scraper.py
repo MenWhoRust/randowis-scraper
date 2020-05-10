8a326d6a24a9b9ba1a5f07d8fb596da27173c4ba
Phabiann-Ross De Carvalho <prdecarvalho96@outlook.com>
2020-05-10 17:31:56 +0200

    cleaned up some code

--- randowis-scraper.py
+++ randowis-scraper.py
@@ -2,60 +2,89 @@ from requests import get
 import sys
 import os
 import re
+import argparse
 from bs4 import BeautifulSoup
 
-currentPage = 1
 
-dirs = os.listdir()
-if not dirs.__contains__('comics'):
-    os.mkdir('comics')
-
-os.chdir('comics')
-while True:
-
-    html = get(f'https://randowis.com/category/short-comics/page/{currentPage}/')
+def get_image_elements(current_page):
+    html = get(f'https://randowis.com/category/short-comics/page/{current_page}/')
 
     if html.status_code != 200:
         print(f'Request returned {html.status_code}. End of comics assumed.')
-        break
+        return None
+    return BeautifulSoup(html.content).find_all('img', {'class': re.compile(r'wp-image-\w+')})
 
-    soup = BeautifulSoup(html.content)
 
-    entries = soup.find_all('img', {'class': re.compile(r'wp-image-\w+')})
-    if len(entries) <= 0:
-        continue
+def create_comics_dir():
+    dirs = os.listdir()
+    if not dirs.__contains__('comics'):
+        os.mkdir('comics')
+    os.chdir('comics')
 
-    dirName = f"Page {currentPage}"
-    os.mkdir(dirName)
 
-    for entry in entries:
-        url = entry.get('src')
-        fileName = entry.get('alt')
-        if fileName == '':
+def download_file(file_name, url):
+    with open(file_name, "wb") as f:
+        print(f'Downloading {file_name}')
+        response = get(url, stream=True)
+        total_length = response.headers.get('content-length')
+
+        if total_length is None:
+            f.write(response.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
+                sys.stdout.flush()
+
+
+def fetch_comics(keep_page_structure):
+    current_page = 1
+
+    while True:
+
+        entries = get_image_elements(current_page)
+        if entries is None or len(entries) <= 0:
+            current_page += 1
             continue
 
-        writePath = os.path.join(os.getcwd(), dirName, fileName)
+        dir_name = f"Page {current_page}" if keep_page_structure else ''
 
-        if not (os.path.exists(writePath) and os.path.isfile(writePath)):
-            print(f'Comic Page {currentPage}/{fileName} found. Skipping...')
-            continue
+        if dir_name != '' and not os.path.exists(dir_name):
+            os.mkdir(dir_name)
 
-        with open(writePath, "wb") as f:
-            print(f'Downloading {fileName}')
-            response = get(url, stream=True)
-            total_length = response.headers.get('content-length')
+        for entry in entries:
+            url = entry.get('src')
+            file_name = entry.get('alt')
+            if file_name == '':
+                continue
 
-            if total_length is None:
-                f.write(response.content)
-            else:
-                dl = 0
-                total_length = int(total_length)
-                for data in response.iter_content(chunk_size=4096):
-                    dl += len(data)
-                    f.write(data)
-                    done = int(50 * dl / total_length)
-                    sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
-                    sys.stdout.flush()
+            write_path = os.path.join(os.getcwd(), dir_name, file_name)
 
-    currentPage += 1
-    pass
+            if os.path.exists(write_path) and os.path.isfile(write_path):
+                print(f'Comic Page {current_page}/{file_name} found. Skipping...')
+                continue
+
+            download_file(write_path, url)
+
+        current_page += 1
+        pass
+
+
+def get_args():
+    ap = argparse.ArgumentParser()
+
+    ap.add_argument('-f', '--first', help='Gets the first comic on the first page', default=False, action='store_true')
+    ap.add_argument('-ks', '--keep-page-structure', default=True, help="Comics will be separated by pages",
+                    action='store_true')
+
+    return vars(ap.parse_args())
+
+
+if __name__ == '__main__':
+    args = get_args()
+    create_comics_dir()
+    fetch_comics(args['keep_page_structure'])
